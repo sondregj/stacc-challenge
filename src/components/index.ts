87ba258916cc60b1ea22340ff2f39784c6e0bb98
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-09-28 13:43:42 +0200

    :sparkles: Stacc SVG

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,3 +7,4 @@ export { default as GraphLegend } from './GraphLegend'
 // Other
 export { default as Currency } from './Currency'
 export { default as InfoPoint } from './InfoPoint'
+export { default as StaccSVG } from './StaccSVG'
