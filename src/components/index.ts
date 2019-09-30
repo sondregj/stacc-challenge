02c41cb6f50ab60845005ee24117b7cf49c96120
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-09-30 09:30:47 +0200

    :sparkles: Completed loan data viewer

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,10 @@ export { default as Footer } from './Footer'
 export { default as Button } from './Button'
 export { default as RangeSlider } from './RangeSlider'
 
+// Data Views
+export { default as LoanData } from './LoanData'
+export { default as PaymentList } from './PaymentList'
+
 // Graph
 export { default as Graph } from './Graph'
 export { default as GraphBar } from './GraphBar'
@@ -14,5 +18,6 @@ export { default as GraphLegend } from './GraphLegend'
 
 // Other
 export { default as Currency } from './Currency'
+export { default as ColorIndicator } from './ColorIndicator'
 export { default as InfoPoint } from './InfoPoint'
 export { default as StaccSVG } from './StaccSVG'
